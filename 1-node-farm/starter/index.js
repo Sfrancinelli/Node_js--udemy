792370019677d4818b5099bcfbdd3316d1d321eb
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-12-01 11:12:53 -0300

    writing and reading files async

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 
+/*
+// Blocking, synchronous way
 const text = fs.readFileSync("./txt/input.txt", "utf-8");
 
 const textOut = `This is what we know about the avocado: ${text}.\nCreated on ${Date.now()}`;
@@ -7,3 +9,19 @@ console.log(textOut);
 
 fs.writeFileSync("./txt/output.txt", textOut);
 console.log("File written!");
+*/
+
+// Non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.error(err);
+      });
+    });
+  });
+});
+console.log("reading file");
