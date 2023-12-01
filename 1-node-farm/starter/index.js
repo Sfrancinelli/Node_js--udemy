1c8074303440893788b9b8cdf9febd6c549890e8
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-12-01 10:31:05 -0300

    Reading and writing files with fs module

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,3 +1,9 @@
 const fs = require("fs");
 
-console.log(fs);
+const text = fs.readFileSync("./txt/input.txt", "utf-8");
+
+const textOut = `This is what we know about the avocado: ${text}.\nCreated on ${Date.now()}`;
+console.log(textOut);
+
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File written!");
