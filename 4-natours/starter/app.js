2537c9eefa798b35e23e0623f14f3806d4967cee
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-12-13 10:42:53 -0300

    Handling post request and persisting the data on the json file

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+app.use(express.json());
 
 /*
 app.get('/', (req, res) => {
@@ -26,6 +26,31 @@ app.get('/api/v1/tours', (req, res) => {
     data: { tours: tours },
   });
 });
+
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) return console.error(err.message);
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
