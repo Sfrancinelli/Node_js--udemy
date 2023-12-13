60abb630e405e81660fdb851f917cccefac2e4c1
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-12-13 17:02:32 -0300

    creating own middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
