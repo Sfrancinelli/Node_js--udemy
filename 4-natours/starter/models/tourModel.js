f9a6a3034c855a1f172c3ac663a0b922616e5339
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-12-27 12:36:06 -0300

    Modelling the Schema for the Tour

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -4,15 +4,57 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
+    trim: true,
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tours must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 0,
+  },
+  ratingsQuantity: {
     type: Number,
     default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a summary'],
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+    trim: true,
+  },
+  images: {
+    type: [String],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: {
+    type: [Date],
   },
 });
 
