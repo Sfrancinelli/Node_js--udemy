1828481bb6db1abc93aad50ba558b74252e760d2
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2024-01-17 14:57:45 -0300

    Added middleware for the global error handling

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const errorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -25,5 +27,22 @@ app.use((req, res, next) => {
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// If the code reaches here it means thtat none of the routers were able to catch the request (unhandled routes)
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Error handler middleware
+app.use(errorHandler);
 
 module.exports = app;
