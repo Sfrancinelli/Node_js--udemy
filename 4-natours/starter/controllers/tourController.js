81c79b9ce20bef1631a30e81e046e7aa3d915983
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2024-01-17 12:21:14 -0300

    refactored APIFeatures class into another folfer

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,6 +1,7 @@
 /* eslint-disable guard-for-in */
 /* eslint-disable no-restricted-syntax */
 const Tour = require('../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.aliasTopTour = (req, res, next) => {
   req.query.limit = '5';
@@ -8,65 +9,6 @@ exports.aliasTopTour = (req, res, next) => {
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
 };
-
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
-
-  filter() {
-    const queryObj = { ...this.queryString };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 2) Advance filtering
-    let queryStr = JSON.stringify(queryObj);
-    // Replacing the operators that come from the query to the MongoDB operators. The operators come from the query without the dollar sign $, so, below, what i did was create a regular expression that finds the operators (gte,gt,lte,lt). It needs to find em exactly (thats why the \b) and it cant find multiples one (thats why the "g" at the end). Once it does the finding, the callback function adds the dollar sign before the matched operator!
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    this.query = this.query.find(JSON.parse(queryStr));
-
-    return this;
-  }
-
-  sort() {
-    if (this.queryString.sort) {
-      // Getting multiple sorting requirements and passing it to the sort function.
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      console.log(sortBy);
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort('-createdAt');
-    }
-
-    return this;
-  }
-
-  limitFields() {
-    if (this.queryString.fields) {
-      // Selecting the fields that will be sent to the client
-      const fields = this.queryString.fields.split(',').join(' ');
-      console.log(fields);
-      this.query = this.query.select(fields);
-    } else {
-      // If no field is selected, the __v fields is removed (thats why the prefix "-")
-      this.query = this.query.select('-__v');
-    }
-
-    return this;
-  }
-
-  paginate() {
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    this.query = this.query.skip(skip).limit(limit);
-
-    return this;
-  }
-}
 
 // HANDLERS
 exports.getAllTours = async (req, res) => {
