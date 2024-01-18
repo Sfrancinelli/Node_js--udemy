f072c885af9f46fccc4e1974a61157911224f15c
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2024-01-18 15:58:04 -0300

    last resource (safety net) for unhandled promise rejections

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -45,6 +45,17 @@ const PORT = process.env.PORT || 3000;
 
 // console.log(process.env.PORT);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
+});
+
+// Every unhandled rejection will be handled here. It's the safety net for the unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION, EXITING PROCESS!');
+  // Closing the server before shutting down the application to give time to the server to finish the request that are being handled at the moment
+  server.close(() => {
+    // Exit the process with code 1 (unhandled rejection)
+    process.exit(1);
+  });
 });
