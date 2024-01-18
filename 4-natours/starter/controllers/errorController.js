bcbcf336c33077700fd20a5ba8c07defe6625608
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2024-01-18 15:17:04 -0300

    Error handling for custom vaidations in (the validations in the tourSchema)

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -16,6 +16,16 @@ const handleDuplicatedFieldsDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  console.log(errors);
+
+  const message = `Innvalid input data. ${errors.join('. ')}`;
+
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -61,6 +71,8 @@ module.exports = (err, req, res, next) => {
     if (err.name === 'CastError') error = handleCastErrorDB(error);
 
     if (err.code === 11000) error = handleDuplicatedFieldsDB(error);
+
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
