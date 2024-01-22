0ed9767ac63646085d18756d6784cc92876e9c27
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2024-01-22 16:24:53 -0300

    created signup and login function with auth and validation. using jwt and bcrypt

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,13 +1,55 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const jwt = require('jsonwebtoken');
+const AppError = require('./../utils/appError');
+
+const signToken = (id) => {
+  return jwt.sign({ id: id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exists
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ email: email }).select('+password');
+  // To select a field that is explicity unselected in the model (to hide it from the get responses), its imperative to write a plus sign before the name of the field
+  console.log(user);
+
+  // Its possible to use the instance method of the userModel because the user in this case its an instance of the model itself (as we use the User.findOne)
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  // 3) If everything ok, send token to client
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });
