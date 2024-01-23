5c391244ee565e4af9957f4ed6be21255d9a50e7
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2024-01-23 13:11:45 -0300

    handling JWT related errors

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -26,6 +26,12 @@ const handleValidationErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = () =>
+  new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpired = () =>
+  new AppError('Your token has expired! Please log in again!', 401);
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -73,6 +79,10 @@ module.exports = (err, req, res, next) => {
     if (err.code === 11000) error = handleDuplicatedFieldsDB(error);
 
     if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
+
+    if (err.name === 'JsonWebTokenError') error = handleJWTError();
+
+    if (err.name === 'TokenExpiredError') error = handleJWTExpired();
 
     sendErrorProd(error, res);
   }
